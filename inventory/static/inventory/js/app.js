c6b3188632136ae3069a65f8c547d2c057049ac6
nfgrawker <nfgrawker@gmail.com>
2019-11-04 19:27:34 -0600

    building out inventory page
    added modals and styling

--- inventory/static/inventory/js/app.js
+++ inventory/static/inventory/js/app.js
@@ -9,7 +9,7 @@ window.onload = function () {
             if ($(this).attr("id") == undefined) {
                 console.log("skipping token")
             } else {
-                objectToSend[$(this).attr("id")] = $(this).val()
+                objectToSend[$(this).attr("id")] = $(this).val();
                 arrayForInput.push($(this).val())
             }
         });
@@ -24,35 +24,47 @@ window.onload = function () {
             },
             dataType: "json"
         }).then(response => {
-            console.log(response)
+            console.log(response);
             if(response['status'] == 1){
-                let newRow = $("<tr>")
+                console.log("adding")
+                let newRow = $("<tr>");
                 arrayForInput.forEach(function(val,idx){
-                    let tableData = $("<td>")
-                    tableData.append(val)
+                    let tableData = $("<td>");
+                    tableData.append(val);
                     newRow.append(tableData)
+                    console.log("appending")
+                    console.log(tableData)
                 });
-                $(".mainInventoryTable").append(newRow)
+                $(".mainInventoryTable").prepend(newRow)
             }
         });
         console.log(objectToSend)
     });
     $("#changeProduct").click(function(){
         parent = $("#postProduct").parent().parent();
+        url = "/changeProduct";
         let allListElements = $("input");
         let inputs = $(parent).find(allListElements);
         inputs.each(function () {
             if ($(this).attr("id") == undefined) {
                 console.log("skipping token")
             } else {
-                objectToSend[$(this).attr("id")] = $(this).val()
+                objectToSend[$(this).attr("td")] = $(this).val()
             }
 
         });
-            keyList = Object.keys(objectToSend)
-            paramsList = []
+            keyList = Object.keys(objectToSend);
+            paramsList = [];
         keyList.forEach(function(val,idx){
-
+            if (idx == 0){
+                url += "?"+val+"="+objectToSend[val]
+            }
+            else{
+                url += "&"+val+"="+objectToSend[val]
+            }
+        });
+        $.get(url,function(response){
+            console.log(response)
         })
     })
 };
