1e559df55785af6c4ba1f547ef7f63c4fab24ec8
nfgrawker <nfgrawker@gmail.com>
2019-11-14 17:05:26 -0600

    Adding customer post route

--- inventory/static/inventory/js/customer.js
+++ inventory/static/inventory/js/customer.js
@@ -1,12 +1,19 @@
 window.onload = function () {
     let selectDropDown = $('#selectCustomer');
     selectDropDown.change(function () {
-        if (selectDropDown.val() == "new"){
-            $('.btn-info').text("Submit")
+        if (selectDropDown.val() == "new") {
+            $('.button').text("Submit")
+        } else {
+            $('.button').text("Change");
         }
-        else{
-            $('.btn-info').text("Change")
 
-        }
+    });
+    $('.button').click(function () {
+        jsonObjectToSend = {};
+        $("input").each(function (index) {
+            jsonObjectToSend[$(this).attr("name")] = $(this).val();
+        });
+        let csrftoken = $("[name=csrfmiddlewaretoken]").val();
+        console.log(jsonObjectToSend)
     })
 };
